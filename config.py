313a07d5d663b74f479391b2ae166c3d1800d1e3
Mohammed Alaghbry <mohammedalaghbry3@gmail.com>
2025-01-26 21:21:34 +0300

    تحديثات جديدة للتطبيق

--- config.py
+++ config.py
@@ -1,12 +1,31 @@
-TELEGRAM_BOT_TOKEN = "7490344438:AAHtSw6V0vTn7i1mIIuGPgJ_PqDTJXKAeos"
+import os
+import asyncpg
+from dotenv import load_dotenv
+load_dotenv()  # تحميل متغيرات البيئة
 
-DEFAULT_CHANNEL_ID = "-1002277553158"  # المعرف الافتراضي للقناة
+
 
 # إعداد اتصال قاعدة البيانات
 DATABASE_CONFIG = {
-    'user': 'exaado_user',
-    'password': 'Moh*770175667',
-    'database': 'exaado_db',
-    'host': 'localhost',
-    'port': 5432
+    'user': os.environ.get('DB_USER', 'exaado_user'),
+    'password': os.environ.get('DB_PASSWORD', 'Moh*770175667'),
+    'database': os.environ.get('DB_NAME', 'exaado_db'),
+    'host': os.environ.get('DB_HOST', 'localhost'),
+    'port': int(os.environ.get('DB_PORT', 5432)),
 }
+
+async def connect_to_db():
+    pool = await asyncpg.create_pool(
+        dsn=DATABASE_URI,
+        command_timeout=60,
+        server_settings={
+            "client_encoding": "UTF8"  # فرض الترميز
+        }
+    )
+    return pool
+
+
+
+DATABASE_URI = f"postgresql://{DATABASE_CONFIG['user']}:{DATABASE_CONFIG['password']}@{DATABASE_CONFIG['host']}:{DATABASE_CONFIG['port']}/{DATABASE_CONFIG['database']}"
+
+TELEGRAM_BOT_TOKEN = "7490344438:AAHtSw6V0vTn7i1mIIuGPgJ_PqDTJXKAeos"
